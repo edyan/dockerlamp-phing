47040e54ca4c8b3b0dc32eab3b10d64f346efc25
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-07-03 08:30:06 +0200

    Plugins files

---
+++ phing.py
@@ -0,0 +1,18 @@
+import click
+import os
+import sys
+
+from lib import command
+
+
+@click.command(help="Run Phing with the build.xml file located in the current directory", name="phing")
+@click.pass_context
+def phing(ctx):
+    # Insert the parent lamp dir in path to get lib/command.py
+    sys.path.insert(0,'../..')
+
+    current_dir = os.getcwd()
+    tty = 't' if sys.stdin.isatty() else ''
+    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', current_dir + ':' + current_dir]
+    cmd += ['inetprocess/phing', 'phing', '-f', current_dir + '/build.xml']
+    command.launch_cmd_displays_output(cmd)
