cec3c71a57d5054bc4a55f3d03b09ca6739d6bd2
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-07-17 19:09:50 +0200

    Bug detecting the current dir

--- phing/core.py
+++ phing/core.py
@@ -8,11 +8,11 @@ from marina import command
 @click.command(help="Run Phing with the build.xml file located in the current directory", name="phing")
 @click.pass_context
 def phing(ctx):
-    # Insert the parent marina dir in path to get lib/command.py
-    sys.path.insert(0, '../..')
+    marina = ctx.obj['MARINA']
+    marina.check_vms_are_running()
 
-    current_dir = os.getcwd()
     tty = 't' if sys.stdin.isatty() else ''
-    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', current_dir + ':' + current_dir]
-    cmd += ['edyan/phing', 'phing', '-f', current_dir + '/build.xml']
+    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', marina.current_dir + ':' + marina.current_dir]
+    cmd += ['edyan/phing', 'phing', '-f', marina.current_dir + '/build.xml']
+
     command.launch_cmd_displays_output(cmd)
