d5b953bbcc3135255a989e53e2a1226e7a8a5083
Emmanuel Dyan <emmanueldyan@gmail.com>
2017-07-23 18:59:07 +0200

    Update core.py

--- phing/core.py
+++ phing/core.py
@@ -2,17 +2,17 @@ import click
 import os
 import sys
 
-from marina import command
+from stakkr import command
 
 
 @click.command(help="Run Phing with the build.xml file located in the current directory", name="phing")
 @click.pass_context
 def phing(ctx):
-    marina = ctx.obj['MARINA']
-    marina.check_vms_are_running()
+    stakkr = ctx.obj['STAKKR']
+    stakkr.check_vms_are_running()
 
     tty = 't' if sys.stdin.isatty() else ''
-    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', marina.current_dir + ':' + marina.current_dir]
-    cmd += ['edyan/phing', 'phing', '-f', marina.current_dir + '/build.xml']
+    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', stakkr.current_dir + ':' + stakkr.current_dir]
+    cmd += ['edyan/phing', 'phing', '-f', stakkr.current_dir + '/build.xml']
 
     command.launch_cmd_displays_output(cmd)
