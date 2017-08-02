6a931c359bfb324999995cdab2aa4df990e00cfd
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-02 18:27:12 +0200

    Compatible with stakkr 3.2

--- phing/core.py
+++ phing/core.py
@@ -1,17 +1,19 @@
 import click
 import sys
 
-from stakkr import command
+from stakkr import command, docker
 
 
 @click.command(help="Run Phing with the build.xml file located in the current directory", name="phing")
 @click.pass_context
 def phing(ctx):
     stakkr = ctx.obj['STAKKR']
-    stakkr.check_cts_are_running()
+    verb = stakkr.context['VERBOSE']
+    debug = stakkr.context['DEBUG']
+    docker.check_cts_are_running(stakkr.project_name, stakkr.config_file)
 
-    print('Pulling image', end='')
-    command.launch_cmd_displays_output(['docker', 'pull', 'edyan/phing'], False, False)
+    print('Pulling image')
+    command.launch_cmd_displays_output(['docker', 'pull', 'edyan/phing'], verb, debug)
     print('Done')
 
     tty = 't' if sys.stdin.isatty() else ''
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='StakkrPhing',
-    version='3.1',
+    version='3.2',
     packages=['phing'],
     entry_points='''
         [stakkr.plugins]
