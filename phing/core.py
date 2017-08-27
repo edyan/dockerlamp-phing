8c7e2c57d535144eccdac1ebba142da107641585
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-27 14:48:09 +0200

    merged

--- phing/core.py
+++ phing/core.py
@@ -1,22 +1,27 @@
-import click
-import sys
+"""
+Phing plugin for stakkr"""
 
+import sys
+import click
 from stakkr import command, docker
 
 
 @click.command(help="Run Phing with the build.xml file located in the current directory", name="phing")
 @click.pass_context
 def phing(ctx):
+    """See command help"""
+
     stakkr = ctx.obj['STAKKR']
     verb = stakkr.context['VERBOSE']
     debug = stakkr.context['DEBUG']
-    docker.check_cts_are_running(stakkr.project_name, stakkr.config_file)
+    docker.check_cts_are_running(stakkr.project_name)
 
     print('Pulling image')
     command.launch_cmd_displays_output(['docker', 'pull', 'edyan/phing'], verb, debug)
     print('Done')
 
     tty = 't' if sys.stdin.isatty() else ''
-    cmd = ['docker', 'run', '-i' + tty, '--rm', '--volume', stakkr.current_dir + ':' + stakkr.current_dir]
-    cmd += ['edyan/phing', 'phing', '-f', stakkr.current_dir + '/build.xml']
+    cmd = ['docker', 'run', '-i' + tty, '--rm']
+    cmd += ['--volume', stakkr.cwd_abs + ':' + stakkr.cwd_abs]
+    cmd += ['edyan/phing', 'phing', '-f', stakkr.cwd_abs + '/build.xml']
     command.launch_cmd_displays_output(cmd, True, True)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='StakkrPhing',
-    version='3.2',
+    version='3.5',
     packages=['phing'],
     entry_points='''
         [stakkr.plugins]
